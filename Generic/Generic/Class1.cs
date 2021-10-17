5c9bbdcc764151f3af639e9771820945741bbc46
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-17 22:29:34 +0530

    Print Array of int ,double and char using generic

---
+++ Generic/Generic/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic
+{
+    class Class1
+    {
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("--------------------------------------");
+        }
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            char[] charArray = { 'h', 'e', 'l', 'l', 'o' };
+            Class1.toPrint<int>( intArray);
+            Class1.toPrint(doubleArray);
+            Class1.toPrint(charArray);
+        }
+    }
+}
